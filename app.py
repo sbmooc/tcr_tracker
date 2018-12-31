4a066f348515a6e39df006eb105db87d11456b44
Oli M-C <o.monaghancoombs@greenrunning.com>
2018-12-31 11:22:37 +0000

    Started work on models and db_interactions. Basic DB design complete and some functionality to read. Currently using local sqlite db

---
+++ app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def test():
+    return jsonify({'hello': 'world'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
