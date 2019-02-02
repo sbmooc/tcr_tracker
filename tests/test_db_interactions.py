86fbac93993042f9814e6cf23b8222e31819e7e9
Oli M-C <o.monaghancoombs@greenrunning.com>
2019-02-02 17:45:53 +0000

    Added new riderRaces model, changed some data stored in riders and trackers data

--- tests/test_db_interactions.py
+++ tests/test_db_interactions.py
@@ -10,7 +10,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import sessionmaker
 
-from models import Base, Riders, PhysicalLocations, Trackers, TrackerLocations
+from models import Base, Riders, PhysicalLocations, Trackers, TrackerLocations, RiderRaces
 from db_interactions import create, get_or_create, get_and_delete, set_up_engine, session_scope, update
 from unittest.mock import patch, Mock
 
@@ -34,7 +34,7 @@ class DBTests(TestCase):
     def setUp(self):
         self.conn = sqlite3.connect(self.temp_db)
         self.cur = self.conn.cursor()
-        # todo ensure that this is turned on for sqlite3, remove once update db?
+        # todo ensure that this is turned on for sqlite3, remove once db is postgres??
         #   https://stackoverflow.com/questions/37034264/sqlite3-on-delete-restrict-via-sqlalchemy-not-being-honored-but-works-fine-in
         # self.cur.execute('PRAGMA foreign_keys=ON')
         # self.conn.commit()
@@ -83,25 +83,19 @@ class TestSetUpDB(DBTests):
         sqlalchemy.orm.Session.rollback.assert_called()
 
 
-class TestManipulatingData(DBTests):
+class TestCRUD(DBTests):
 
     def test_create(self):
         data_to_add = {
             'id': 1,
             'first_name': 'Bobby',
             'last_name': 'Hill',
-            'cap_number': '123',
-            'category': 'solo',
-            'status': 'finished',
             'gender': 'male',
-            'deposit_status': 'paid',
-            'deposit_amount': 123.1,
             'email': 'hello@bob.com'
         }
         create(self.test_session, Riders, **data_to_add)
         data_in_riders = self.cur.execute('SELECT * FROM RIDERS;').fetchall()[0]
-        expected_results_riders = (1, 'Bobby', 'Hill', '123',
-                            'solo', 'finished', 'male', 'paid', 123.1, 'hello@bob.com')
+        expected_results_riders = (1, 'Bobby', 'Hill', 'male', 'hello@bob.com')
         data_in_tracker_locations = self.cur.execute('SELECT * FROM tracker_locations').fetchall()[0]
         expected_results_locations = (1, 'Riders')
         self.assertEqual(data_in_riders, expected_results_riders)
@@ -112,18 +106,12 @@ class TestManipulatingData(DBTests):
             'id': 1,
             'first_name': 'Bobby',
             'last_name': 'Hill',
-            'cap_number': '123',
-            'category': 'solo',
-            'status': 'finished',
             'gender': 'male',
-            'deposit_status': 'paid',
-            'deposit_amount': 123.1,
             'email': 'hello@bob.com'
         }
         test_get = get_or_create(self.test_session, Riders, **data_to_get)
         data_in_riders = self.cur.execute('SELECT * FROM RIDERS;').fetchall()[0]
-        expected_results_riders = (1, 'Bobby', 'Hill', '123',
-                                   'solo', 'finished', 'male', 'paid', 123.1, 'hello@bob.com')
+        expected_results_riders = (1, 'Bobby', 'Hill', 'male', 'hello@bob.com')
         data_in_tracker_locations = self.cur.execute('SELECT * FROM tracker_locations').fetchall()[0]
         expected_results_locations = (1, 'Riders')
         self.assertEqual(data_in_riders, expected_results_riders)
@@ -137,16 +125,11 @@ class TestManipulatingData(DBTests):
             'id': 1,
             'first_name': 'Bobby',
             'last_name': 'Hill',
-            'cap_number': '123',
-            'category': 'solo',
-            'status': 'finished',
             'gender': 'male',
-            'deposit_status': 'paid',
-            'deposit_amount': 123.1,
             'email': 'hello@bob.com'
         }
         params = tuple(test_data.values())
-        query = "INSERT INTO riders VALUES (?,?,?,?,?,?,?,?,?,?)"
+        query = "INSERT INTO riders VALUES (?,?,?,?,?)"
         self.cur.execute(query, params)
         # due to relationship between tables, manually add data to tracker_locations too
         tracker_locations_test_data = {'id': 1, 'type': 'Riders'}
@@ -168,7 +151,9 @@ class TestManipulatingData(DBTests):
         tracker = {
             'tkr_number': 123456,
             'esn_number': '123456',
-            'current_status': 'working',
+            'working_status': 'working',
+            'loan_status': 'not_loaned',
+            'deposit_amount': 100.0,
             'last_test_date': datetime(2017, 1, 1),
             'purchase': datetime(2015, 1, 2),
             'warranty_expiry': datetime(2019, 1, 2),
@@ -178,7 +163,7 @@ class TestManipulatingData(DBTests):
         get_or_create(self.test_session, PhysicalLocations, **physical_location)
         get_or_create(self.test_session, Trackers, **tracker)
         data_in_trackers = self.cur.execute('SELECT * FROM TRACKERS;').fetchall()[0]
-        expected_results = (123456, '123456', 'working', '2017-01-01 00:00:00.000000',
+        expected_results = (123456, '123456', 'working', 'not_loaned', 100.0, '2017-01-01 00:00:00.000000',
                             '2015-01-02 00:00:00.000000', '2019-01-02 00:00:00.000000', 'lost_dot', None, 1)
         self.assertEqual(data_in_trackers, expected_results)
 
@@ -189,7 +174,9 @@ class TestManipulatingData(DBTests):
         tracker = {
             'tkr_number': 123456,
             'esn_number': '123456',
-            'current_status': 'working',
+            'working_status': 'working',
+            'loan_status': 'with_rider',
+            'deposit_amount': 100.0,
             'last_test_date': datetime(2017, 1, 1),
             'purchase': datetime(2015, 1, 2),
             'warranty_expiry': datetime(2019, 1, 2),
@@ -197,7 +184,7 @@ class TestManipulatingData(DBTests):
         }
         get_or_create(self.test_session, Trackers, **tracker)
         data_in_db = self.cur.execute('SELECT * FROM TRACKERS;').fetchall()[0]
-        expected_results = (123456, '123456', 'working', '2017-01-01 00:00:00.000000',
+        expected_results = (123456, '123456', 'working', 'with_rider', 100.0, '2017-01-01 00:00:00.000000',
                             '2015-01-02 00:00:00.000000', '2019-01-02 00:00:00.000000', 'lost_dot', None, None)
         self.assertEqual(data_in_db, expected_results)
 
@@ -207,7 +194,7 @@ class TestManipulatingData(DBTests):
             'category': 'nonsense'
         }
         with self.assertRaises(IntegrityError):
-            create(self.test_session, Riders, **data_to_add)
+            create(self.test_session, RiderRaces, **data_to_add)
 
     def test_get_and_delete(self):
         physical_location = {
@@ -217,7 +204,9 @@ class TestManipulatingData(DBTests):
         tracker = {
             'tkr_number': 123456,
             'esn_number': '123456',
-            'current_status': 'working',
+            'working_status': 'working',
+            'loan_satus': 'not_loaned',
+            'deposit_amount': 100.0,
             'last_test_date': datetime(2017, 1, 1),
             'purchase': datetime(2015, 1, 2),
             'warranty_expiry': datetime(2019, 1, 2),
@@ -227,7 +216,7 @@ class TestManipulatingData(DBTests):
         }
 
         self.cur.execute('INSERT INTO physical_locations VALUES (?, ?)', tuple(physical_location.values()))
-        self.cur.execute('INSERT INTO trackers VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', tuple(tracker.values()))
+        self.cur.execute('INSERT INTO trackers VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', tuple(tracker.values()))
         self.conn.commit()
 
         get_and_delete(self.test_session, Trackers, **{'tkr_number': 123456})
@@ -244,7 +233,9 @@ class TestManipulatingData(DBTests):
         tracker_1 = {
             'tkr_number': 123456,
             'esn_number': '123456',
-            'current_status': 'working',
+            'working_status': 'working',
+            'loan_status': 'not_loaned',
+            'deposit_amount': 100.00,
             'last_test_date': datetime(2017, 1, 1),
             'purchase': datetime(2015, 1, 2),
             'warranty_expiry': datetime(2019, 1, 2),
@@ -255,7 +246,9 @@ class TestManipulatingData(DBTests):
         tracker_2 = {
             'tkr_number': 123457,
             'esn_number': '123456',
-            'current_status': 'working',
+            'working_status': 'working',
+            'loan_status': 'not_loaned',
+            'deposit_amount': 100.00,
             'last_test_date': datetime(2017, 1, 1),
             'purchase': datetime(2015, 1, 2),
             'warranty_expiry': datetime(2019, 1, 2),
@@ -266,7 +259,9 @@ class TestManipulatingData(DBTests):
         tracker_3 = {
             'tkr_number': 123458,
             'esn_number': '123456',
-            'current_status': 'working',
+            'working_status': 'working',
+            'loan_status': 'not_loaned',
+            'deposit_amount': 100.00,
             'last_test_date': datetime(2017, 1, 1),
             'purchase': datetime(2015, 1, 2),
             'warranty_expiry': datetime(2019, 1, 2),
@@ -275,9 +270,9 @@ class TestManipulatingData(DBTests):
             'location_id': 1,
         }
         self.cur.execute('INSERT INTO physical_locations VALUES (?, ?)', tuple(physical_location.values()))
-        self.cur.execute('INSERT INTO trackers VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', tuple(tracker_1.values()))
-        self.cur.execute('INSERT INTO trackers VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', tuple(tracker_2.values()))
-        self.cur.execute('INSERT INTO trackers VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', tuple(tracker_3.values()))
+        self.cur.execute('INSERT INTO trackers VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', tuple(tracker_1.values()))
+        self.cur.execute('INSERT INTO trackers VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', tuple(tracker_2.values()))
+        self.cur.execute('INSERT INTO trackers VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', tuple(tracker_3.values()))
         self.conn.commit()
         get_and_delete(self.test_session, Trackers, commit=False, **{'tkr_number': 123456})
         get_and_delete(self.test_session, Trackers, commit=False, **{'tkr_number': 123457})
@@ -291,12 +286,14 @@ class TestManipulatingData(DBTests):
 
         self.cur.execute('INSERT INTO tracker_locations VALUES (?, ?)',
                          (1, 'Riders'))
-        self.cur.execute('INSERT INTO RIDERS VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                         (1, 'bob', 'hill', '123', 'solo', 'scratched', 'male', None, None, None))
+        self.cur.execute('INSERT INTO RIDERS VALUES (?, ?, ?, ?, ?)',
+                         (1, 'bob', 'hill', 'male', None))
         tracker = {
             'tkr_number': 123458,
             'esn_number': '123456',
-            'current_status': 'working',
+            'working_status': 'working',
+            'loan_status': 'with_rider',
+            'deposit_amount': 123.0,
             'last_test_date': datetime(2017, 1, 1),
             'purchase': datetime(2015, 1, 2),
             'warranty_expiry': datetime(2019, 1, 2),
@@ -304,7 +301,7 @@ class TestManipulatingData(DBTests):
             'third_party_name': None,
             'location_id': 1,
         }
-        self.cur.execute('INSERT INTO trackers VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+        self.cur.execute('INSERT INTO trackers VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
                          tuple(tracker.values()))
         self.conn.commit()
         # attempt to delete tracker location, even when there is a tracker associated with it
@@ -330,7 +327,9 @@ class TestManipulatingData(DBTests):
         tracker = {
             'tkr_number': 123456,
             'esn_number': '123456',
-            'current_status': 'working',
+            'working_status': 'working',
+            'loan_status': 'not_loaned',
+            'deposit_amount': 100,
             'last_test_date': datetime(2017, 1, 1),
             'purchase': datetime(2015, 1, 2),
             'warranty_expiry': datetime(2019, 1, 2),
@@ -339,29 +338,27 @@ class TestManipulatingData(DBTests):
             'location_id': 1,
         }
         self.cur.execute('INSERT INTO physical_locations VALUES (?, ?)', tuple(physical_location.values()))
-        self.cur.execute('INSERT INTO trackers VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', tuple(tracker.values()))
+        self.cur.execute('INSERT INTO trackers VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', tuple(tracker.values()))
         self.conn.commit()
         filter_ = {'tkr_number': 123456}
         updates = {'owner': 'third_party',
                    'third_party_name': 'TAW'}
         update(self.test_session, Trackers, updates, **filter_)
         result = self.cur.execute('SELECT * FROM trackers').fetchall()
-        self.assertEqual(result[0], (123456, '123456', 'working',  '2017-01-01 00:00:00',
+        self.assertEqual(result[0], (123456, '123456', 'working',  'not_loaned', 100, '2017-01-01 00:00:00',
                                      '2015-01-02 00:00:00', '2019-01-02 00:00:00',
                                      'third_party', 'TAW', 1))
 
 
-class ManiuplateDataUsingSetUp(DBTests):
-
-# todo something weird going on with this, won't run following other tests :-/
+class TestCRUDUsingContextMgr(DBTests):
 
     def setUp(self):
-        super(ManiuplateDataUsingSetUp, self).setUp()
+        super(TestCRUDUsingContextMgr, self).setUp()
         os.environ['DB_TYPE'] = 'sqlite:///'
         os.environ['DB_URI'] = self.temp_db
 
     def tearDown(self):
-        super(ManiuplateDataUsingSetUp, self).tearDown()
+        super(TestCRUDUsingContextMgr, self).tearDown()
         del os.environ['DB_TYPE']
         del os.environ['DB_URI']
 
